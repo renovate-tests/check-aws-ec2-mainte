d35d8b53e79fe87815478b78c75a5ff2e4f22b03
ntrv <hydrangea56@me.com>
2019-03-14 17:57:12 +0900

    Set timeout to metadata

--- lib/metadata.go
+++ lib/metadata.go
@@ -1,15 +1,42 @@
 package checkawsec2mainte
 
 import (
+	"net/http"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/ec2metadata"
 	"github.com/aws/aws-sdk-go/aws/session"
 )
 
-func getInstanceIdFromMetadata() (string, error) {
-	metadata := ec2metadata.New(session.New())
+func getInstanceIdFromMetadata() string {
+	metadata := ec2metadata.New(session.New(), &aws.Config{
+		HTTPClient: &http.Client{Timeout: 100 * time.Millisecond},
+	})
+
+	if !metadata.Available() {
+		return ""
+	}
+
 	d, err := metadata.GetInstanceIdentityDocument()
 	if err != nil {
-		return "", err
+		return ""
 	}
-	return d.InstanceID, nil
+	return d.InstanceID
+}
+
+func getRegionFromMetadata() string {
+	metadata := ec2metadata.New(session.New(), &aws.Config{
+		HTTPClient: &http.Client{Timeout: 100 * time.Millisecond},
+	})
+
+	if !metadata.Available() {
+		return ""
+	}
+
+	region, err := metadata.Region()
+	if err != nil {
+		return ""
+	}
+	return region
 }
