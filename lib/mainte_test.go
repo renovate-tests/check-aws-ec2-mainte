1a4c2825202755b839da6633446935f454114859
ntrv <hydrangea56@me.com>
2019-03-15 19:25:38 +0900

    Add tests

--- lib/mainte_test.go
+++ lib/mainte_test.go
@@ -8,9 +8,23 @@ import (
 )
 
 func TestTimeIsOver(t *testing.T) {
+	past, _ := time.Parse(time.RFC3339, "2006-01-02T15:04:05+07:00")
+	future, _ := time.Parse(time.RFC3339, "2018-03-15T19:14:05+09:00")
+
 	mainte := EC2Mainte{
-		NotBefore: time.Now(),
+		NotBefore: past,
 	}
 
-	assert.True(t, mainte.IsTimeOver(time.Now(), 1*time.Hour))
+	assert.True(t, mainte.IsTimeOver(future, 1*time.Hour))
+}
+
+func TestTimeIsNotOver(t *testing.T) {
+	past, _ := time.Parse(time.RFC3339, "2006-01-02T15:04:05+09:00")
+	future, _ := time.Parse(time.RFC3339, "2006-01-02T15:04:05+07:00")
+
+	mainte := EC2Mainte{
+		NotBefore: future,
+	}
+
+	assert.False(t, mainte.IsTimeOver(past, 1*time.Hour))
 }
