32f62c6d4bb9cecfa9f3f00471b7cfcb712dcfd0
ntrv <hydrangea56@me.com>
2019-03-16 23:27:53 +0900

    Add test for Completed

---
+++ lib/events_test.go
@@ -0,0 +1,68 @@
+package checkawsec2mainte
+
+import (
+	"testing"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/stretchr/testify/assert"
+)
+
+func createTimes(t *testing.T, values []string) (ds []time.Time) {
+	for _, v := range values {
+		d, err := time.Parse(time.RFC3339, v)
+		if err != nil {
+			t.Fatal(err)
+		}
+		ds = append(ds, d)
+	}
+	return
+}
+
+func createEvents(t *testing.T) EC2Events {
+
+	ds := createTimes(t, []string{
+		"2019-03-14T16:04:05+09:00",
+		"2019-03-16T16:04:05+09:00",
+		"2019-03-16T18:04:05+09:00",
+		"2019-03-17T17:34:35+09:00",
+		"2019-03-17T18:04:05+09:00",
+		"2019-03-17T18:04:05+07:00",
+	})
+
+	return EC2Events{
+		{
+			Code:        ec2.EventCodeSystemReboot,
+			InstanceId:  "i-9263d590",
+			NotBefore:   ds[2],
+			Description: "scheduled reboot",
+		},
+		{
+			Code:        ec2.EventCodeSystemMaintenance,
+			InstanceId:  "i-07bfa293eacde7019",
+			NotBefore:   ds[0],
+			NotAfter:    ds[1],
+			Description: "[Completed] Scheduled System Maintenance",
+		},
+		{
+			Code:        ec2.EventCodeInstanceRetirement,
+			InstanceId:  "i-05d9be9a",
+			NotBefore:   ds[5],
+			Description: "[Completed] Scheduled Instance Retirement Maintenance",
+		},
+		{
+			Code:        ec2.EventCodeInstanceReboot,
+			InstanceId:  "i-0f456b937f33abe9e",
+			NotBefore:   ds[3],
+			NotAfter:    ds[4],
+			Description: "Scheduled Instance Reboot Maintenance",
+		},
+	}
+}
+
+func TestFilterCompleted(t *testing.T) {
+	events := createEvents(t)
+
+	events = events.Filter("Completed")
+	assert.Len(t, events, 2)
+}
