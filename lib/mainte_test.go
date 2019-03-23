3add87f9bc5c619641d3f6a9018edd8c3dd1002f
ntrv <hydrangea56@me.com>
2019-03-23 17:21:05 +0900

    Split test cases

--- lib/mainte_test.go
+++ lib/mainte_test.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"testing"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	"github.com/aws/aws-sdk-go-v2/service/ec2/ec2iface"
 	"github.com/k0kubun/pp"
@@ -12,30 +11,6 @@ import (
 	"github.com/ntrv/check-aws-ec2-mainte/lib/unit"
 	"github.com/stretchr/testify/assert"
 )
-
-func TestGetMaintesFromAPI(t *testing.T) {
-	cases := createCases(t)
-
-	for _, c := range cases {
-		mt := checkawsec2mainte.EC2Mainte{
-			Client:      mockEc2Svc{Resp: c.Resp},
-			InstanceIds: []string{},
-		}
-		events, err := mt.GetMainteInfo(context.Background())
-		if err != nil {
-			t.Error(err)
-		}
-
-		pp.Println(events)
-
-		assert.Equal(t, c.Expected, events)
-	}
-}
-
-type testCaseMainte struct {
-	Resp     ec2.DescribeInstanceStatusOutput
-	Expected checkawsec2mainte.EC2Events
-}
 
 type mockEc2Svc struct {
 	ec2iface.EC2API
@@ -50,68 +25,21 @@ func (m mockEc2Svc) DescribeInstanceStatusRequest(input *ec2.DescribeInstanceSta
 	}
 }
 
-func createCases(t *testing.T) []testCaseMainte {
-	ds := unit.CreateTimes(t, []string{
-		"2019-03-14T16:04:05+09:00",
-		"2019-03-16T16:04:05+09:00",
-		"2019-03-16T18:04:05+09:00",
-	})
+func TestGetMaintesFromAPI(t *testing.T) {
+	cases := unit.CreateCases(t)
 
-	return []testCaseMainte{
-		{
-			Resp: ec2.DescribeInstanceStatusOutput{
-				InstanceStatuses: []ec2.InstanceStatus{
-					{
-						InstanceId: aws.String("i-0472b8a82f226da14"),
-						Events: []ec2.InstanceStatusEvent{
-							{
-								Code:        ec2.EventCodeSystemMaintenance,
-								Description: aws.String("Scheduled System Maintenance"),
-								NotBefore:   aws.Time(ds[0]),
-								NotAfter:    aws.Time(ds[1]),
-							},
-						},
-					},
-					{
-						InstanceId: aws.String("i-0dc818ea941b1ae18"),
-						Events: []ec2.InstanceStatusEvent{
-							{
-								Code:        ec2.EventCodeInstanceRetirement,
-								Description: aws.String("Scheduled Instance Retirement Maintenance"),
-								NotBefore:   aws.Time(ds[2]),
-								NotAfter:    aws.Time(ds[2]), //TODO: Check this field is required
-							},
-						},
-					},
-					{
-						InstanceId: aws.String("i-0dc818ea941b1ae18"),
-						Events: []ec2.InstanceStatusEvent{
-							{
-								Code:        ec2.EventCodeInstanceReboot,
-								Description: aws.String("[Completed] Scheduled Instance Reboot Maintenance"),
-								NotBefore:   aws.Time(ds[0]),
-								NotAfter:    aws.Time(ds[1]),
-							},
-						},
-					},
-				},
-			},
-			Expected: checkawsec2mainte.EC2Events{
-				{
-					Code:        ec2.EventCodeSystemMaintenance,
-					InstanceId:  "i-0472b8a82f226da14",
-					Description: "Scheduled System Maintenance",
-					NotBefore:   ds[0],
-					NotAfter:    ds[1],
-				},
-				{
-					Code:        ec2.EventCodeInstanceRetirement,
-					InstanceId:  "i-0dc818ea941b1ae18",
-					Description: "Scheduled Instance Retirement Maintenance",
-					NotBefore:   ds[2],
-					NotAfter:    ds[2], //TODO: Check this field is required
-				},
-			},
-		},
+	for _, c := range cases {
+		mt := checkawsec2mainte.EC2Mainte{
+			Client:      mockEc2Svc{Resp: c.Resp},
+			InstanceIds: []string{},
+		}
+		events, err := mt.GetMainteInfo(context.Background())
+		if err != nil {
+			t.Error(err)
+		}
+
+		pp.Println(events)
+
+		assert.Equal(t, c.Expected, events)
 	}
 }
---
+++ lib/unit/mainte.go
@@ -0,0 +1,80 @@
+package unit
+
+import (
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	checkawsec2mainte "github.com/ntrv/check-aws-ec2-mainte/lib"
+)
+
+type testCaseMainte struct {
+	Resp     ec2.DescribeInstanceStatusOutput
+	Expected checkawsec2mainte.EC2Events
+}
+
+func CreateCases(t *testing.T) []testCaseMainte {
+	ds := CreateTimes(t, []string{
+		"2019-03-14T16:04:05+09:00",
+		"2019-03-16T16:04:05+09:00",
+		"2019-03-16T18:04:05+09:00",
+	})
+
+	return []testCaseMainte{
+		{
+			Resp: ec2.DescribeInstanceStatusOutput{
+				InstanceStatuses: []ec2.InstanceStatus{
+					{
+						InstanceId: aws.String("i-0472b8a82f226da14"),
+						Events: []ec2.InstanceStatusEvent{
+							{
+								Code:        ec2.EventCodeSystemMaintenance,
+								Description: aws.String("Scheduled System Maintenance"),
+								NotBefore:   aws.Time(ds[0]),
+								NotAfter:    aws.Time(ds[1]),
+							},
+						},
+					},
+					{
+						InstanceId: aws.String("i-0dc818ea941b1ae18"),
+						Events: []ec2.InstanceStatusEvent{
+							{
+								Code:        ec2.EventCodeInstanceRetirement,
+								Description: aws.String("Scheduled Instance Retirement Maintenance"),
+								NotBefore:   aws.Time(ds[2]),
+								NotAfter:    aws.Time(ds[2]), //TODO: Check this field is required
+							},
+						},
+					},
+					{
+						InstanceId: aws.String("i-0dc818ea941b1ae18"),
+						Events: []ec2.InstanceStatusEvent{
+							{
+								Code:        ec2.EventCodeInstanceReboot,
+								Description: aws.String("[Completed] Scheduled Instance Reboot Maintenance"),
+								NotBefore:   aws.Time(ds[0]),
+								NotAfter:    aws.Time(ds[1]),
+							},
+						},
+					},
+				},
+			},
+			Expected: checkawsec2mainte.EC2Events{
+				{
+					Code:        ec2.EventCodeSystemMaintenance,
+					InstanceId:  "i-0472b8a82f226da14",
+					Description: "Scheduled System Maintenance",
+					NotBefore:   ds[0],
+					NotAfter:    ds[1],
+				},
+				{
+					Code:        ec2.EventCodeInstanceRetirement,
+					InstanceId:  "i-0dc818ea941b1ae18",
+					Description: "Scheduled Instance Retirement Maintenance",
+					NotBefore:   ds[2],
+					NotAfter:    ds[2], //TODO: Check this field is required
+				},
+			},
+		},
+	}
+}
