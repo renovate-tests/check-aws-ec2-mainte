d09cd739fbdfe2583e5e1ca64daaa51aa9edbe0e
ntrv <hydrangea56@me.com>
2019-05-10 11:33:45 +0900

    Update Formula

--- Formula/check-aws-ec2-mainte.rb
+++ Formula/check-aws-ec2-mainte.rb
@@ -3,7 +3,7 @@ class CheckAwsEc2Mainte < Formula
   homepage 'https://github.com/ntrv/check-aws-ec2-mainte'
   version '0.0.11'
   sha256 'f2b80b971c68ba87b8deaf03cfb743781e945aeee82016c3b3c8e2669207c9a4'
-  url 'https://github.com/ntrv/check-aws-ec2-mainte/releases/download/v0.0.11/check-aws-ec2-mainte_darwin_amd64.zip'
+  url 'https://github.com/ntrv/check-aws-ec2-mainte/releases/download/v0.1.1/check-aws-ec2-mainte_darwin_amd64.zip'
   head 'https://github.com/ntrv/check-aws-ec2-mainte.git'
 
   def install
