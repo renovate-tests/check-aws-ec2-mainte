aa957c87439ab288ca829146b775a50c635b0a3d
ntrv <hydrangea56@me.com>
2019-09-19 15:44:13 +0900

    Bump up version

--- Formula/check-aws-ec2-mainte.rb
+++ Formula/check-aws-ec2-mainte.rb
@@ -1,9 +1,9 @@
 class CheckAwsEc2Mainte < Formula
   desc 'Mackerel plugin to detect EC2 Retirement'
   homepage 'https://github.com/ntrv/check-aws-ec2-mainte'
-  version '0.1.1'
+  version '0.1.2'
   sha256 '515e7fd5a53f5db4f6b2f789965317837beaec19c1ba122cbc85c8c5f5f71c00'
-  url 'https://github.com/ntrv/check-aws-ec2-mainte/releases/download/v0.1.1/check-aws-ec2-mainte_darwin_amd64.zip'
+  url 'https://github.com/ntrv/check-aws-ec2-mainte/releases/download/v0.1.2/check-aws-ec2-mainte_darwin_amd64.zip'
   head 'https://github.com/ntrv/check-aws-ec2-mainte.git'
 
   def install
